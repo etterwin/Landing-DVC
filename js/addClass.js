8f9c41d8ee9aa32e3f93686a557875f5ca4e3377
Dasha <etterwin.yll@gmail.com>
2017-11-01 16:04:41 +0300

    update

--- js/addClass.js
+++ js/addClass.js
@@ -17,3 +17,70 @@ function actionFlag() {
         });
     });
 }
+
+function actionIcons() {
+
+    $(document).ready(function() {
+        let windowHeight = $(window).height();
+
+        $(document).on('scroll', function() {
+            $('#action_icons').each(function() {
+                let self = $(this),
+                    height = self.offset().top + self.height();
+                if ($(document).scrollTop() + windowHeight >= height) {
+                    document.getElementById("circle_1").classList.add("network-circle-1--small");
+                    document.getElementById("circle_2").classList.add("network-circle");
+                    document.getElementById("circle_3").classList.add("network-circle-2--small");
+                    document.getElementById("circle_4").classList.add("network-circle-1--big");
+                    document.getElementById("circle_5").classList.add("network-circle-2--big");
+                    document.getElementById("circle_6").classList.add("network-circle-3--small");
+                    document.getElementById("earth").classList.add("network-earth");
+                    document.getElementById("sym").classList.add("network-sym");
+                    document.getElementById("chat").classList.add("network-chat");
+                    document.getElementById("light").classList.add("network-light");
+                    document.getElementById("loud").classList.add("network-loud");
+                    document.getElementById("coop").classList.add("network-coop");
+                    document.getElementById("email").classList.add("network-email");
+                }
+            });
+        });
+    });
+}
+
+function actionSybmols() {
+
+    $(document).ready(function() {
+        let windowHeight = $(window).height();
+
+        $(document).on('scroll', function() {
+            $('#action_symbols').each(function() {
+                let self = $(this),
+                    height = self.offset().top + self.height();
+                if ($(document).scrollTop() + windowHeight >= height) {
+                    document.getElementById("percent_1").classList.add("progress-percent--left");
+                    document.getElementById("bulb_1").classList.add("progress-bulb--left");
+                    document.getElementById("doll_1").classList.add("progress-doll--left");
+                    document.getElementById("percent_2").classList.add("progress-percent");
+                    document.getElementById("euro_1").classList.add("progress-euro--left");
+                    document.getElementById("bulb_2").classList.add("progress-bulb");
+                    document.getElementById("euro_2").classList.add("progress-eur--right");
+                    document.getElementById("percent_3").classList.add("progress-percent--right");
+                    document.getElementById("bulb_3").classList.add("progress-bulb--right");
+                    document.getElementById("doll_2").classList.add("progress-doll--right");
+
+
+                    document.getElementById("percent_4").classList.add("progress-percent--left");
+                    document.getElementById("bulb_4").classList.add("progress-bulb--left");
+                    document.getElementById("doll_4").classList.add("progress-doll--left");
+                    document.getElementById("percent_5").classList.add("progress-percent");
+                    document.getElementById("euro_4").classList.add("progress-euro--left");
+                    document.getElementById("bulb_5").classList.add("progress-bulb");
+                    document.getElementById("euro_5").classList.add("progress-eur--right");
+                    document.getElementById("percent_6").classList.add("progress-percent--right");
+                    document.getElementById("bulb_6").classList.add("progress-bulb--right");
+                    document.getElementById("doll_5").classList.add("progress-doll--right");
+                }
+            });
+        });
+    });
+}
